c2771eeb5fd08a93030018dbe83904b5add2e202
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-12-19 18:22:56 +1100

    UnsupportedTypeJsonNodeException serialVersionUID

--- src/main/java/walkingkooka/tree/json/UnsupportedTypeJsonNodeException.java
+++ src/main/java/walkingkooka/tree/json/UnsupportedTypeJsonNodeException.java
@@ -22,6 +22,8 @@ package walkingkooka.tree.json;
  */
 public class UnsupportedTypeJsonNodeException extends JsonNodeException {
 
+    private static final long serialVersionUID = 1L;
+
     protected UnsupportedTypeJsonNodeException() {
         super();
     }

9a71bb18c33241aa356e3080ae21a93881c1dcda
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-26 23:12:59 +1000

    JsonObjectList implements ImmutableList

--- src/test/java/walkingkooka/tree/json/JsonObjectListTest.java
+++ src/test/java/walkingkooka/tree/json/JsonObjectListTest.java
@@ -19,11 +19,55 @@ package walkingkooka.tree.json;
 
 import org.junit.jupiter.api.Test;
 import walkingkooka.ToStringTesting;
-import walkingkooka.collect.list.ListTesting2;
+import walkingkooka.collect.list.ImmutableListTesting;
 import walkingkooka.collect.map.Maps;
 
-public final class JsonObjectListTest implements ListTesting2<JsonObjectList, JsonNode>,
+public final class JsonObjectListTest implements ImmutableListTesting<JsonObjectList, JsonNode>,
         ToStringTesting<JsonObjectList> {
+
+    @Test
+    public void testJsonObject() {
+        JsonObject object = JsonNode.object();
+
+        final JsonPropertyName first = JsonPropertyName.with("first");
+        final JsonPropertyName second = JsonPropertyName.with("second");
+
+        final JsonNode value1 = JsonNode.booleanNode(true);
+        final JsonNode value2 = JsonNode.number(22);
+
+        object = object.set(
+                first,
+                value1
+        ).set(
+                second,
+                value2
+        );
+
+        this.checkEquals(
+                JsonObjectList.with(
+                        Maps.of(
+                                first,
+                                value1.setName(first),
+                                second,
+                                value2.setName(second)
+                        )
+                ),
+                object.children
+        );
+
+        this.checkNotEquals(
+                JsonObjectList.with(
+                        Maps.of(
+                                first,
+                                value1,
+                                second,
+                                value2
+                        )
+                ),
+                object.children,
+                () -> "same name/value but values without name should not be equalb f"
+        );
+    }
 
     @Test
     public void testToString() {
@@ -32,8 +76,19 @@ public final class JsonObjectListTest implements ListTesting2<JsonObjectList, Js
 
     @Override
     public JsonObjectList createList() {
-        return JsonObjectList.with(Maps.of(JsonPropertyName.with("first"), JsonNode.booleanNode(true),
-                JsonPropertyName.with("second"), JsonNode.booleanNode(false)));
+        final JsonPropertyName first = JsonPropertyName.with("first");
+        final JsonPropertyName second = JsonPropertyName.with("second");
+
+        return JsonObjectList.with(
+                Maps.of(
+                        first,
+                        JsonNode.booleanNode(true)
+                                .setName(first),
+                        second,
+                        JsonNode.booleanNode(false)
+                                .setName(second)
+                )
+        );
     }
 
     @Override

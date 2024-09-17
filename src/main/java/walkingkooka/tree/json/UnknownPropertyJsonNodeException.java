99a316e2a8bd4a25b564f2b1d4c0f19de2f22d28
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-09-17 20:06:01 +1000

    UnknownPropertyJsonNodeException "Unknown property X in Y"

--- src/main/java/walkingkooka/tree/json/UnknownPropertyJsonNodeException.java
+++ src/main/java/walkingkooka/tree/json/UnknownPropertyJsonNodeException.java
@@ -35,6 +35,6 @@ public final class UnknownPropertyJsonNodeException extends PropertyJsonNodeExce
 
     @Override
     String messagePrefix() {
-        return "Unknown";
+        return "Unknown property";
     }
 }

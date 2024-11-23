5f3453922beff06c7610c55b8e005bfb0b281c03
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-11-23 19:33:37 +1100

    Inherit java.util.Base64

--- src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -2,7 +2,11 @@ package test;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 import walkingkooka.j2cl.locale.LocaleAware;
+import walkingkooka.text.CharSequences;
 import walkingkooka.tree.json.JsonNode;
 
 @LocaleAware
@@ -25,6 +29,21 @@ public class TestGwtTest extends GWTTestCase {
                 JsonNode.booleanNode(true),
                 JsonNode.parse(
                         "true"
+                )
+        );
+    }
+
+    public void testEncodeDecodeRoundtrip() {
+        final String data = "abc123def456";
+        final Charset charset = Charset.forName("UTF-8");
+
+        final String encoded = Base64.getEncoder().encodeToString(data.getBytes(charset));
+        assertEquals("Started with " + CharSequences.quoteAndEscape(data) + " encoded: " + CharSequences.quoteAndEscape(encoded),
+                data,
+                new String(
+                        Base64.getDecoder()
+                                .decode(encoded),
+                        charset
                 )
         );
     }

abaead62f27feb4ecb1182067fa0be66ea0b039c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-02 20:44:57 +1100

    JsonNodeExpressionFunction

---
+++ src/main/java/walkingkooka/tree/json/expression/function/JsonNodeExpressionFunction.java
@@ -0,0 +1,39 @@
+package walkingkooka.tree.json.expression.function;
+
+import walkingkooka.tree.expression.ExpressionFunctionName;
+import walkingkooka.tree.expression.ExpressionPurityContext;
+import walkingkooka.tree.expression.function.ExpressionFunction;
+import walkingkooka.tree.json.expression.JsonNodeExpressionEvaluationContext;
+
+import java.util.Optional;
+
+abstract class JsonNodeExpressionFunction<C extends JsonNodeExpressionEvaluationContext, T> implements ExpressionFunction<T, C> {
+
+    JsonNodeExpressionFunction(final String name) {
+        super();
+
+        this.name = Optional.of(
+            ExpressionFunctionName.with(name)
+        );
+    }
+
+    @Override
+    public final Optional<ExpressionFunctionName> name() {
+        return this.name;
+    }
+
+    private final Optional<ExpressionFunctionName> name;
+
+    @Override
+    public final boolean isPure(final ExpressionPurityContext expressionPurityContext) {
+        return true; // JsonNode functions are always pure
+    }
+
+    // Object...........................................................................................................
+
+    @Override
+    public final String toString() {
+        return this.name.get()
+            .toString();
+    }
+}

a593da72c65a3e761b9ddeb0afeb9a835217b151
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-08-29 19:52:33 +1000

    JsonNodeExpressionFunctionJsonPointer
    - Closes https://github.com/mP1/walkingkooka-tree-json/issues/510
    - ExpressionFunction: jsonPointer(String)

---
+++ src/main/java/walkingkooka/tree/json/expression/function/JsonNodeExpressionFunctionJsonPointer.java
@@ -0,0 +1,53 @@
+package walkingkooka.tree.json.expression.function;
+
+import walkingkooka.Cast;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameter;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameterKind;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameterName;
+import walkingkooka.tree.json.expression.JsonNodeExpressionEvaluationContext;
+import walkingkooka.tree.json.pointer.JsonPointer;
+
+import java.util.List;
+
+/**
+ * A function that returns a {@link JsonPointer}.
+ */
+final class JsonNodeExpressionFunctionJsonPointer<C extends JsonNodeExpressionEvaluationContext> extends JsonNodeExpressionFunction<C, JsonPointer> {
+
+    /**
+     * Type safe getter.
+     */
+    static <C extends JsonNodeExpressionEvaluationContext> JsonNodeExpressionFunctionJsonPointer<C> instance() {
+        return Cast.to(INSTANCE);
+    }
+
+    final static JsonNodeExpressionFunctionJsonPointer<?> INSTANCE = new JsonNodeExpressionFunctionJsonPointer<>();
+
+
+    private JsonNodeExpressionFunctionJsonPointer() {
+        super("jsonPointer");
+    }
+
+    @Override
+    public List<ExpressionFunctionParameter<?>> parameters(final int count) {
+        return PARAMETERS;
+    }
+
+    final static ExpressionFunctionParameter<JsonPointer> POINTER = ExpressionFunctionParameterName.with("pointer")
+        .required(JsonPointer.class)
+        .setKinds(ExpressionFunctionParameterKind.CONVERT_EVALUATE);
+
+    final static List<ExpressionFunctionParameter<?>> PARAMETERS = ExpressionFunctionParameter.list(POINTER);
+
+    @Override
+    public Class<JsonPointer> returnType() {
+        return JsonPointer.class;
+    }
+
+    @Override
+    public JsonPointer apply(final List<Object> parameters,
+                             final C context) {
+        checkParameterCount(parameters);
+        return POINTER.getOrFail(parameters, 0);
+    }
+}
